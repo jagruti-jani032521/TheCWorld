dcd9267fd9de8d631b82d7611114d2112c53c38a
jagruti-jani032521 <jagrutijani25@gmail.com>
2026-05-04 23:08:54 +0530

    operator

---
+++ ch - 3 operator & expression/unary p-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+
+{
+	
+	int n = 979;
+   
+   printf("%d\t" ,++n);
+   printf("%d\t" ,--n);
+   printf("%d\t" ,n--);
+   printf("%d\t" ,++n);
+   printf("%d\t" ,--n);
+   printf("%d" ,n++);
+   
+}
