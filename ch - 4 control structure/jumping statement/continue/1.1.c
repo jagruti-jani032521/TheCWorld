df5b3172cd5a04dd162340f544ca361d431f2258
jagruti-jani032521 <jagrutijani25@gmail.com>
2026-05-29 17:58:01 +0530

    looping with pattern

--- ch - 4 control structure/jumping statement/continue/1.1.c
+++ ch - 4 control structure/jumping statement/continue/1.1.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+2w-#include <stdio.h>
 
 main()
 {
---
+++ ch - 5 looping with pattern/without space/1.9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+main()
+{
+	int i,j;
+	
+	for(i=5; i>=1; i--)
+	{
+		
+		for(j=i; j<=5; j++)
+		{
+		   (i%2)? printf("0") : printf("1");
+		}
+	printf("\n");
+	}
+}
